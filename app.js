c4e176edc7c7ae377ed345099b812593c48c927b
MARIA LUIZA GOMES DE ALMEIDA <23111819@CFP127ALUNO.LOCAL>
2023-09-01 17:24:35 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const chats = [
+    {
+        cliente: 'Jerome Bueno',
+        mensagem:'Posso realizar o pagamento por...',
+        atendente: 'João Paulo'
+    },
+    {
+        cliente: 'Daniel Silva',
+        mensagem:'Márcia diz: Bom dia, como posso ajuda...',
+        atendente: 'João Paulo'
+    },
+]
